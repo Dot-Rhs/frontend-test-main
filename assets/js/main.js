341442a7c61fb4faf41f8b6e2a4c4f1e7b809b78
RhysH <rhys.handley@santander.co.uk>
2022-06-24 14:08:36 +0100

    - Added HTML comments - Added JS comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
-var navBtn = document.getElementById('hamburger')
+var navBtn = document.getElementById('hamburger');
 
-navBtn.addEventListener('click', function (e) {
-	document.body.classList.toggle('is-active');
-})
+// navBtn.addEventListener('click', function (e) {
+// 	document.body.classList.toggle('is-active');
+// })
+
+// This is fine, can swap to ES6 syntax though, not really an issue though:
+
+navBtn.addEventListener('click', e => {
+  document.body.classList.toggle('is-active');
+});
+
+// You *could* swap the event listener for an onClick in the html, then moving the eventlistener logic in your own function, thats called onclick.
+// I think they picked the eventListener intentionally though, some benefits of using the eventListener route is that you can tie multiple listeners to a single target, where as onclick can only ever have one function.
+// Using the eventlistener helps keep code separate, HTML in HTML (no onClick) and JS in JS etc.
